6945a1ba8090d6b904fdcb631c0767452494225d
hyppis1 <147988668+hyppis1@users.noreply.github.com>
2025-01-22 02:55:01 +0200

    Plugin to make custom sounds to happen when you miss or hit an attack

--- src/main/java/com/YouMissedThatOne/YouMissedThatOnePlugin.java
+++ src/main/java/com/YouMissedThatOne/YouMissedThatOnePlugin.java
@@ -20,7 +20,6 @@ import net.runelite.api.ChatMessageType;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
